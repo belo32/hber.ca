1cfce7b13d95d23a96f39f7b6d928504bb201dff
Bilal Al <bilal@server.com>
2013-01-19 18:32:19 -0500

    added new services and seperated out the index page to individual pages

---
+++ assets/js/gallery-viewer.js
@@ -0,0 +1,60 @@
+/**
+* JQuery script for loading images from the facebook page album providing the album id as follows:
+* <div class="fb-album" data-id="{ALBUM_ID}"></div>
+* images will be added in the following format <div class="fb-image"><img src="{image_source}"/>
+* Author: Bilal Al-Hajjar
+*/
+/**
+* This is customized for the view of hber.ca
+*/
+var facebookUrl = "https://graph.facebook.com/";
+var photosExt = "/photos?fields=source";
+$.support.cors = true;
+$(document).ready(function(){
+	var id =$('.fb-album').attr('data-id');
+	var albumUrl = facebookUrl + id + photosExt 
+	getAjaxJSON(albumUrl);
+
+        
+
+});
+
+function getAjaxJSON(albumUrl){
+$.ajax({
+    url: albumUrl,
+    dataType: "json",
+    type: "GET",
+    success: function(data) {
+        
+        var dataArray = data.data;
+        $.each(dataArray, function(i,item){
+        	console.log(item.source);
+        	
+
+            var ref =$("<a></a>").attr({
+                class:"fb-image grid_4",
+                
+                href: item.source,
+                sytle: "background-image:url("+item.source+")"
+                });
+        	$("<img/>").attr("src",item.source).appendTo(ref);
+				ref.appendTo(".fb-album");
+        });
+
+        $(".fb-album a").touchTouch(); 
+
+        console.log(dataArray);
+        
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        console.log("error "+ textStatus);
+        
+    },
+    complete: function(data){
+    	console.log("complete");
+    }
+	});
+
+}
+
+
