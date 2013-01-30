198cac3fdca0d1cf0bf32c300d8f44ddd0585edf
Bilal Al <bilal@server.com>
2013-01-30 00:10:06 -0500

    fix 1

--- assets/js/gallery-viewer.js
+++ assets/js/gallery-viewer.js
@@ -7,7 +7,7 @@
 /**
 * This is customized for the view of hber.ca
 */
-
+if (!(window.console && console.log)) { (function() { var noop = function() {}; var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn']; var length = methods.length; var console = window.console = {}; while (length--) { console[methods[length]] = noop; } }()); }
 var facebookUrl = "http://graph.facebook.com/";
 var photosExt = "/photos?fields=source";
 jQuery.support.cors = true;
