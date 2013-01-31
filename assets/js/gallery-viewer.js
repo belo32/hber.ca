bc0704bb7618f34ceccb108bf6372b21169fe503
Bilal Al <bilal@server.com>
2013-01-30 22:40:18 -0500

    fix 1

--- assets/js/gallery-viewer.js
+++ assets/js/gallery-viewer.js
@@ -9,8 +9,8 @@
 */
 var facebookUrl = "http://graph.facebook.com/";
 var photosExt = "/photos?fields=source";
-  <script>if (!(window.console && console.log)) { (function() { var noop = function() {}; var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn']; var length = methods.length; var console = window.console = {}; while (length--) { console[methods[length]] = noop; } }()); }
-</script>
+if (!(window.console && console.log)) { (function() { var noop = function() {}; var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn']; var length = methods.length; var console = window.console = {}; while (length--) { console[methods[length]] = noop; } }()); }
+
 jQuery.support.cors = true;
 $(document).ready(function(){
 	var id =$('.fb-album').attr('data-id');
